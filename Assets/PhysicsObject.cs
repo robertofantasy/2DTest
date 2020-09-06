60e4b32e115cbce34a89c0ad099b13c8bd0972cd
rafiee <rafiee@DESKTOP-BU749TM>
2020-09-06 16:08:53 +0430

    Checking Collision
    till now i write code to check character collision

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifyer = 1f;
+    public float minGroundNormalY = .65f;
+
+    protected bool Grounded;
+    protected Vector2 GroundNormal;
+    protected Vector2 velocity;
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitbufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+        
+    }
+
+
+    // Update is called once per frame
+
+
+    private void FixedUpdate()
+    {
+        velocity += Physics2D.gravity * gravityModifyer * Time.deltaTime;
+        Vector2 deltaposition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaposition.y;
+        Grounded = false;
+
+        Movement(move, true);
+
+        
+    }
+
+    void Movement(Vector2 move, bool yMovemeny)
+    {
+        float distance = move.magnitude;
+
+        if(distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitbufferList.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                hitbufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitbufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitbufferList[i].normal;
+                if(currentNormal.y < minGroundNormalY)
+                {
+                    Grounded = true;
+
+                    if(yMovemeny)
+                    {
+                        GroundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+            }
+            
+        }
+
+        rb2d.position += rb2d.position + move;
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Test : MonoBehaviour
+{
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] RayBuffer = new RaycastHit2D[16];
+    protected float ShellRadius = 0.001f;
+    public Collider2D sdd;
+    public Vector3 start;
+    public Vector3 End;
+
+
+    public float GravityModifier = 1;
+    public float MinDistance = 0.001f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+        sdd = this.gameObject.GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += Physics2D.gravity * GravityModifier * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaPosition.y;
+        Movement(move);
+        //Debug.DrawLine(new Vector2(0,0), new Vector2(5,5), Color.red);
+        
+;    }
+
+
+    private void Update()
+    {
+        
+        Debug.DrawLine(start, End, Color.red, 100f);
+    }
+
+    private void Movement(Vector2 move)
+    {
+        float Distance = move.magnitude;
+        if(Distance > MinDistance)
+        {
+           int count = rb2d.Cast(move, contactFilter, RayBuffer, Distance + ShellRadius);
+        }
+        rb2d.position =rb2d.position + move;
+    }
+}
